3f22e4dba4fc10bdac4effa20caca8fec89eb7a4
teddyPark <sneezer77@naver.com>
2022-01-24 18:57:00 +0900

    added hive auto repair partition option

--- dump_hive_ddl.sh
+++ dump_hive_ddl.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-JDBC_URL="jdbc:hive2://localhost:10000"
+JDBC_URL="jdbc:hive2://master2:10000"
+DB_NAME=$1".hql"
 rm -f tableNames.txt
-rm -f HiveTableDDL.txt
+rm -f $DB_NAME
 beeline -u $JDBC_URL --showHeader=false --outputformat=tsv2 -e "use $1; show tables;" > tableNames.txt
 #hive -e "use $1; show tables;" > tableNames.txt  
 wait
-echo -e "create database if not exists $1; use $1" >>HiveTableDDL.txt
+echo -e "create database if not exists $1; use $1;" >>$DB_NAME
 cat tableNames.txt |while read LINE
    do
-   echo -e "drop table if exists $LINE;\n" >>HiveTableDDL.txt
-   beeline -u $JDBC_URL --showHeader=false --outputformat=tsv2 -e "use $1; show create table $LINE;" | sed "s/\"/\\\\\"/g" >>HiveTableDDL.txt
-   echo -e ";\n" >> HiveTableDDL.txt
+   echo -e "drop table if exists $LINE;\n" >>$DB_NAME
+   beeline -u $JDBC_URL --showHeader=false --outputformat=tsv2 -e "use $1; show create table $LINE;" | sed "s/\"/\\\\\"/g" >>$DB_NAME
+   echo -e ";\n" >>$DB_NAME
    done
 rm -f tableNames.txt
 echo "Table DDL generated"
